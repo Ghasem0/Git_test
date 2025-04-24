50b2299d3d922f05232ae11fc2edd481f70cb1a0
Ghasem Yalameh <115513132+Ghasem0@users.noreply.github.com>
2025-04-24 17:58:15 +0330

    Update main.py

--- class_2/main.py
+++ class_2/main.py
@@ -1,7 +1,7 @@
 print("i written in github")
-<<<<<<< HEAD
+
 print("its add after pull")
-=======
+
 
 print("i after pulling written in github")
->>>>>>> 612a1c2a72d2df790695f82b1addb02e1c8c1b52
+
